71d33b8e17c7bbd7ac9a4cdba0f1a6a2a79073a1
陈张萌 <chenzm17@mails.tsinghua.edu.cn>
2022-05-09 02:29:07 +0800

    未登录状态下不显示退出登录

--- src/components/DirectBar.vue
+++ src/components/DirectBar.vue
@@ -3,14 +3,19 @@
     <!-- <goto-button :myname="Login" :myurl="login"></goto-button>
     <goto-button :myname="Regist" :myurl="regist"></goto-button> -->
     <!-- <goto-button :myname="Login" :myurl="login"></goto-button> -->
-    <h3>Login</h3>
-    <ul>
-        <li><a href="/login" rel="noopener">Login</a></li>
-    </ul>
-        <h3>Regist</h3>
-    <ul>
-        <li><a href="/regist" rel="noopener">Regist</a></li>
-    </ul>
+
+    <!-- <h3>display: flex</h3> -->
+    <div class="display-flex">
+    <div class="div-size">
+            <a href="/login" rel="noopener">登陆</a>
+    </div>
+    <div class="div-size">
+            <a href="/regist" rel="noopener">注册</a>
+    </div>
+    <!-- <div class="div-size">3</div> -->
+    </div>
+    
+        
 
 </div>
 </template>
--- src/components/LogoutPage.vue
+++ src/components/LogoutPage.vue
@@ -4,10 +4,16 @@
     <div v-html="problem"></div>
     <p><img :src="image" ></p>
 
-    <!-- 输入框 -->
-    <form v-on:submit.prevent="submit()">
+    <!-- <h3 v-if="my_status==true">您已登陆！</h3>
+    <h3 v-else>您未登录！</h3> -->
+    <form v-on:submit.prevent="submit()" v-if="my_status==true">
       <tr><button type="submit" class="ui-button" ><span>退出登录</span></button></tr>
     </form>
+
+    <!-- 输入框 -->
+    <!-- <form v-on:submit.prevent="submit()">
+      <tr><button type="submit" class="ui-button" ><span>退出登录</span></button></tr>
+    </form> -->
 
   </div>
 </template>
@@ -20,9 +26,18 @@ export default {
   data(){
     return {
         username: this.username,
+        my_status: this.my_status,
     }
   },
   created(){
+      axios.get('/login_status')
+              .then((res) => {
+            //   console.log(res.data)
+              this.my_username=res.data['username']
+              this.my_status=res.data['status']
+            //   console.log(this.my_username)
+            //   console.log(this.my_status)
+        })
   },
     methods: {
       submit() {
